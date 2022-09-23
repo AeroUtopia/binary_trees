4ffda42e9f51fd14d57b68f27dedd46fab13684d
Semachew Fasika <eth.saf.1992@gmail.com>
2022-09-23 16:35:48 +0300

    Fixing some errors

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,54 +1,48 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- *
- * @tree: tree root
- * Return: size of the tree or 0 if tree is NULL;
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
+ * leaf_check - Check if a node is a leaf
+  * @node: Node to check
+   * Return: 1 if leaf, 0 if not
+    **/
+    int leaf_check(const binary_tree_t *node)
+    {
+    	if (node == NULL)
+			return (0);
+				if (node->left == NULL && node->right == NULL)
+						return (1);
+							return (0);
+							}
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
+							/**
+							 * binary_tree_is_complete - Check if a tree is complete
+							  * @tree: The root of the tree to check
+							   * Return: 1 if tree is complete, 0 if it's not complete
+							    **/
+							    int binary_tree_is_complete(const binary_tree_t *tree)
+							    {
+							    	binary_tree_t *left_child, *right_child;
 
-/**
- * tree_is_complete - checks if tree is complete
- *
- * @tree: pointer to the tree root
- * @i: node index
- * @cnodes: number of nodes
- * Return: 1 if tree is complete, 0 otherwise
- */
-int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (i >= cnodes)
-		return (0);
-
-	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
-		tree_is_complete(tree->right, (2 * i) + 2, cnodes));
-}
-
-
-/**
- * binary_tree_is_complete - calls to tree_is_complete function
- *
- * @tree: tree root
- * Return: 1 if tree is complete, 0 otherwise
- */
-int binary_tree_is_complete(const binary_tree_t *tree)
-{
-	size_t cnodes;
-
-	if (tree == NULL)
-		return (0);
-
-	cnodes = binary_tree_size(tree);
-
-	return (tree_is_complete(tree, 0, cnodes));
-}
+									if (tree == NULL)
+											return (0);
+												if (tree->left == NULL && tree->right == NULL)
+														return (1);
+															left_child = tree->left;
+																right_child = tree->right;
+																	if (right_child != NULL)
+																		{
+																				if ((right_child->left != NULL && right_child->right != NULL) &&
+																						    (left_child->left == NULL || left_child->right == NULL))
+																						    			return (0);
+																										}
+																											if (left_child != NULL)
+																												{
+																														if ((left_child->left != NULL && left_child->right != NULL) &&
+																																    (right_child->left == NULL || right_child->right == NULL))
+																																    			return (0);
+																																				}
+																																					if (leaf_check(right_child) && leaf_check(left_child))
+																																							return (1);
+																																								return (binary_tree_is_complete(left_child) &&
+																																										binary_tree_is_complete(right_child));
+																																										}
